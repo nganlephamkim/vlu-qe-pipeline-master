8583a411174716f220a3c57bf62b3f14add0aea5
nganlephamkim <ngan.lephamkim@gmail.com>
2026-09-21 13:35:17 +0700

    auth.smoke.test.js

---
+++ auth.smoke.test.js
@@ -0,0 +1,18 @@
+const login = require("./auth");
+
+test("Mat khau sai tra ve false", () => {
+    expect(login("admin", "456")).toBe(false);
+});
+
+test("Username rong tra ve false", () => {
+    expect(login("", "123")).toBe(false);
+});
+
+test("Mat khau chua ky tu dac biet tra ve false", () => {
+    expect(login("admin", "12@3")).toBe(false);
+});
+
+test("Tai khoan bi khoa tra ve false", () => {
+    expect(login("locked", "123")).toBe(false);
+});
+
