2c313aed3dff120a62f0fff74c631ce93df8fb9c
nganlephamkim <ngan.lephamkim@gmail.com>
2026-09-21 13:30:53 +0700

    auth.js

---
+++ auth.js
@@ -0,0 +1,27 @@
+function login(username, password) {
+    // Tai khoan bi khoa
+    if (username === "locked") {
+        return false;
+    }
+
+    // Dang nhap dung
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    // Username rong
+    if (username === "") {
+        return false;
+    }
+
+    // Mat khau chua ky tu dac biet
+    if (/[^a-zA-Z0-9]/.test(password)) {
+        return false;
+    }
+
+    // Sai username hoac password
+    return false;
+}
+
+module.exports = login;
+
